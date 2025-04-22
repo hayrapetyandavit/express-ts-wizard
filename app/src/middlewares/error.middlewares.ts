835e7b99ffe8ea081dd83eae147b05c375687ff0
Davit Hayrapetyan <122399094+hayrapetyandavit@users.noreply.github.com>
2025-04-22 21:46:39 +0400

    Revert "hold"

--- app/src/middlewares/error.middlewares.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function errorHandler(error: Error, _req: Request, res: Response, next: NextFunction) {
-    console.log(error)
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-  
